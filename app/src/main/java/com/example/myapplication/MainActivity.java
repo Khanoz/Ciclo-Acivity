a0478eb45981cfacd788b53eaa480aebfb4b0036
Khanoz <81890326+Khanoz@users.noreply.github.com>
2022-01-27 17:27:37 -0500

    app

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,26 +16,30 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        //Log.d("OnStart", "Hola");
+        Log.d("OnStart", "Hola");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        Log.d("onResume", "Hola");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        Log.d("onPause", "Hola");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+        Log.d("onStop", "Hola");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        Log.d("onDestroy", "Hola");
     }
 }
